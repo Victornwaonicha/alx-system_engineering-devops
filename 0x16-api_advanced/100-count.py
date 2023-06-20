c01523c9cb7faa7ee7543409ac6f5c8fe3bc3d9b
Victor <victornwaonicha@gmail.com>
2023-06-20 04:01:02 +0100

    Task3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit
-   that are found in a list
-"""
+"""Contains the count_words function"""
 import requests
 
 
-def count_words(subreddit, word_list, array=None, after=""):
-    if array is None:
-        array = {}
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"after": after}
-    data = requests.get(url, headers=headers, params=params,
-                        allow_redirects=False)
+def count_words(subreddit, word_list, found_list=[], after=None):
+    '''Prints counts of given words found in hot posts of a given subreddit.
 
-    if data.status_code == 200:
-        results = data.json().get("data")
-        after = results.get("after")
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        found_list (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+    '''
+    user_agent = {'User-agent': 'test45'}
+    posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
+                         .format(subreddit, after), headers=user_agent)
+    if after is None:
+        word_list = [word.lower() for word in word_list]
 
-        for entry in results.get("children"):
-            split = entry.get("data").get("title").lower().split()
-
-            for word in word_list:
-                if word.lower() in split:
-                    times = len([t for t in split if t == word.lower()])
-                    if array.get(word) is None:
-                        array[word] = times
-                    else:
-                        array[word] += times
-
-        if after:
-            count_words(subreddit, word_list, array, after)
+    if posts.status_code == 200:
+        posts = posts.json()['data']
+        aft = posts['after']
+        posts = posts['children']
+        for post in posts:
+            title = post['data']['title'].lower()
+            for word in title.split(' '):
+                if word in word_list:
+                    found_list.append(word)
+        if aft is not None:
+            count_words(subreddit, word_list, found_list, aft)
         else:
-            sorted_results = sorted(array.items(), key=lambda x: (-x[1],
-                                    x[0].lower()))
-            for word, count in sorted_results:
-                print("{}: {}".format(word.lower(), count))
+            result = {}
+            for word in found_list:
+                if word.lower() in result.keys():
+                    result[word.lower()] += 1
+                else:
+                    result[word.lower()] = 1
+            for key, value in sorted(result.items(), key=lambda item: item[1],
+                                     reverse=True):
+                print('{}: {}'.format(key, value))
+    else:
+        return
